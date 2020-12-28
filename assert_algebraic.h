fb0fb9d3cd81420931031bfe8d9cd000bf1c1574
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-12-28 17:23:28 +0600

    Link the GitHub repository (assert_algebraic.h)

--- assert_algebraic.h
+++ assert_algebraic.h
@@ -19,6 +19,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+// GitHub: https://github.com/Hirrolot/assert-algebraic.
+
 #ifndef ASSERT_ALGEBRAIC_H
 #define ASSERT_ALGEBRAIC_H
 
